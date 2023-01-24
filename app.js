3bd7b03ae5a05a7c2abf611448d200ec11a896b6
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-01-24 08:25:12 +0530

    .

--- app.js
+++ app.js
@@ -149,5 +149,5 @@ app.get("/about", function(req, res){
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started succesfully");
 });
